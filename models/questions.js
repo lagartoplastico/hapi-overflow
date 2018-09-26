643225efaacc6d34e091787b522812ab369305f3
Adrian Estrada <edsadr@gmail.com>
2018-09-26 11:50:21 -0500

    Métodos de servidor - respuesta correcta

--- models/questions.js
+++ models/questions.js
@@ -29,8 +29,25 @@ class Questions {
 
   async answer (data, user) {
     const answers = await this.collection.child(data.id).child('answers').push()
-    answers.set({text: data.answer, user: user})
+    answers.set({ text: data.answer, user: user })
     return answers
+  }
+
+  async setAnswerRight (questionId, answerId, user) {
+    const query = await this.collection.child(questionId).once('value')
+    const question = query.val()
+    const answers = question.answers
+
+    if (!user.email === question.owner.email) {
+      return false
+    }
+
+    for (let key in answers) {
+      answers[key].correct = (key === answerId)
+    }
+
+    const update = await this.collection.child(questionId).child('answers').update(answers)
+    return update
   }
 }
 
